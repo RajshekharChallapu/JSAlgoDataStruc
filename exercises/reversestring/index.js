49df3e5b526bd4a698aef9eef6988978b09c6526
CR <rajshekhar@protonmail.com>
2018-05-22 07:55:52 -0500

    reverse string solution # 2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -3,21 +3,13 @@
 // Examples   reverse('apple') === 'leppa'   reverse('hello') === 'olleh'
 // reverse('Greetings!') === '!sgniteerG'
 
-/* solution #1 */
+/* solution # 2*/
 function reverse(str){
-    const arr = str.split('');
-    arr.reverse();
-    return arr.join('');
-
-    //concise code for above soultion #1
-    /* function reverse(str){
-        return str
-        .split('')
-        //helper method
-        .reverse()
-        .join('');
-    } */
-
+    //ES2015
+    let reversed='';
+    for (let character of str){
+        reversed = character + reversed;
+    }
+    return reversed;
 }
-
 module.exports= reverse;
